600f34c523e55ccf3b4c5cdbdabc802132a19ea4
Marek Koza <qyx@krtko.org>
2015-04-08 01:41:03 +0200

    ant-routing: Add neighbor discovery.

--- UmeshSimNodeAnt.py
+++ UmeshSimNodeAnt.py
@@ -13,15 +13,41 @@ class UmeshSimNodeAnt(UmeshSimNodeImpl):
 		# Assing random node identifier.
 		self._id = random.randint(0, 256 * 256 * 256 * 256)
 
+		self._neighbors = {}
+
 	def process(self):
 		super(UmeshSimNodeAnt, self).process()
 		self._time += 1
 
+		# Send route discovery message
 		if self._time % 10 == 0:
 			self.send(json.dumps({
+				"src": self._id,
 				"dst": "broadcast",
-				"message": "nbdiscovery",
-				"id": self._id
+				"type": "nbdiscovery"
 			}))
 
+		while self.haveMessages():
+			msg = json.loads(self.receive())
+			if msg["type"] == "nbdiscovery":
+				# Add the neighbor with zero time
+				self._neighbors[msg["src"]] = 0
 
+		nb_to_delete = []
+		for k in self._neighbors.iterkeys():
+			# Increment time for all neighbors
+			self._neighbors[k] += 1
+			if self._neighbors[k] > 20:
+				# And remove those which are older than 2 seconds
+				nb_to_delete.append(k)
+
+		for k in nb_to_delete:
+			del self._neighbors[k]
+
+
+	def tooltip(self):
+		nb = ""
+		for k in self._neighbors.iterkeys():
+			nb += "id = %d, time = %d<br>" % (k, self._neighbors[k])
+
+		return "ant<br>time = %d<br><b>neighbors:</b><br>%s" % (self._time, nb)
